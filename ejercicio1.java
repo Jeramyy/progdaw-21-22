d1dfed876c57c2f4302704cdd1cf2fc190081988
Jeramy Bósquez Domínguez <91059604+Jeramyy@users.noreply.github.com>
2021-10-18 12:07:04 +0200

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,11 @@
+public class ejercicio1 {
+  public static void main(String[] args) {
+    System.out.printf("%-10s %-10s\n","nature" ,"naturaleza");
+    System.out.printf("%-10s %-10s\n","say" ,"decir");
+    System.out.printf("%-10s %-10s\n","choose" ,"elegir");
+    System.out.printf("%-10s %-10s\n","wait" ,"esperar");
+    System.out.printf("%-10s %-10s\n","mountain" ,"montaña");
+    System.out.printf("%-10s %-10s\n","day" ,"dia");
+    System.out.printf("%-10s %-10s\n","save" ,"guardar");
+  }
+}
---
+++ prueba6.java
@@ -0,0 +1,5 @@
+public class t1ejercicio8 {
+  public static void main (String[]args) {
+      Scanner x = new Scanner (System.in);
+  }
+}
