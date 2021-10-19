4627be2059e5ce47bef7a2d8c3510b176be1b750
Jeramy Bósquez Domínguez <91059604+Jeramyy@users.noreply.github.com>
2021-10-19 08:34:02 +0200

    Add files via upload

--- tema4/ejercicio20.java
+++ tema4/ejercicio20.java
@@ -1,15 +1,33 @@
-public class pruebaaaaaas {
+public class ejercicio20 {
   public static void main(String[] args) {
+   int n;
+   boolean capicua = false;
+   System.out.print("Escribe un número y te diré si es capicua");
+   String linea;
+   linea = System.console().readLine();
+   n = Integer.parseInt(linea);
+   if (n <= 9 ) {
+     capicua = true; }
+     if ((n >= 10 ) && (n <=99)) {
+       if ((n / 10 ) == (n % 10)){
+         capicua = true; ;} }
+         if ((n >= 100) && (n <= 999)) {
+           if ((n / 100)  == (n % 10)){
+             capicua = true; } }
+             if ((n >= 1000) && (n <= 9999)) {
+                if (((n / 1000)  == (n % 10)) && (((n/100) % 10)  == ((n % 100) / 10))){
+                      capicua = true; } }
+                      if ((n >= 10000) && (n <= 99999)) {
+                        if ((n / 10000)  == (n % 10)){
+                          if (((n / 10000) == (n % 10) ) && ((((n / 1000) % 10)) == ((n / 10) % 10)))
+                            capicua = true; } }
+             
+        if (capicua) {
+        System.out.println("El número introducido es capicúa.");
+          } else {
+            System.out.println("El número introducido no es capicúa.");
+        }
 
-    System.out.print("Ingrese un número máximo de 5 dígitos: ");
-    String linea;
-    linea = System.console().readLine();
-        int numero;
-    numero = Integer.parseInt(linea);
-    if (
-    
-    
-    
     
     
   }
